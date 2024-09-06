eeea53b548e648dcbe9733bb360b0eb9904ba711
aubegeo <geoffreyabt@gmail.com>
2024-09-06 06:37:38 +0200

    Initial commit on develop branch

---
+++ pages/blog.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Mon Blog</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.slug">
+        <nuxt-link :to="'/blog/' + post.slug">{{ post.title }}</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    const posts = await $content('posts').fetch()
+    return { posts }
+  }
+}
+</script>
